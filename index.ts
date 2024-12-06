f6906aee63c2f0ac6973a52ab59257747a6aa2f0
sylor <sylor8@gmail.com>
2024-12-06 17:28:03 +0800

    fix

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/constant/types';
-import PumpFunProgram from './src/pump/pfpProgram';
-export default PumpFunProgram;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './constant/types';
+import PumpFunProgram from './pump/pfpProgram';
+export default PumpFunProgram;
